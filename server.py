ee65bf88d642254b13b934ea7099248d6189a325
nemoymoy <nemoymoy@gmail.com>
2025-11-12 22:34:56 +0400

    fix bug in server

--- server.py
+++ server.py
@@ -54,7 +54,10 @@ class UserView(MethodView):
 
     def post(self):
         json_data = validate(CreateUser, request.json)
-        user = User(username=json_data['username'], password=hash_password(json_data['password']))
+        user = User(
+            username=json_data['username'],
+            password=hash_password(json_data['password']),
+            email=json_data['email'])
         add_user(user)
         return jsonify(user.id_json)
 
@@ -86,56 +89,56 @@ app.add_url_rule('/user/',
                  methods=['POST']
                  )
 
-def get_ad(ad_id: int) -> Ad:
-    ad = request.session.get(Ad, ad_id)
-    if ad is None:
-        raise HttpError(404, message='ad not found')
-    return ad
-
-def add_ad(ad: Ad):
-    request.session.add(ad)
-    try:
-        request.session.commit()
-    except IntegrityError:
-        raise HttpError(status_code=409, message='ad already exists with the same data')
-
-class AdView(MethodView):
-    def get(self, ad_id: int):
-        ad = get_ad(ad_id)
-        return jsonify(ad.json)
-
-    def post(self):
-        json_data = validate(CreateAd, request.json)
-        ad = Ad(header=json_data['header'], description=json_data['password'], user_id=['user_id'])
-        add_ad(ad)
-        return jsonify(ad.id_json)
-
-    def patch(self, ad_id: int):
-        json_data = validate(PatchAd, request.json)
-        ad = get_ad(ad_id)
-        if 'header' in json_data:
-            ad.header = json_data['header']
-        if 'description' in json_data:
-            ad.description = json_data['description']
-        add_ad(ad)
-        return jsonify(ad.id_json)
-
-    def delete(self, ad_id: int):
-        ad = get_ad(ad_id)
-        request.session.delete(ad)
-        request.session.commit()
-        return jsonify({'status': 'advertisement delete success'})
-
-ad_view = AdView.as_view('ad_view')
-
-app.add_url_rule('/ad/<int:ad_id>',
-                 view_func=ad_view,
-                 methods=['GET', 'PATCH', 'DELETE']
-                 )
-app.add_url_rule('/ad/',
-                 view_func=ad_view,
-                 methods=['POST']
-                 )
+# def get_ad(ad_id: int) -> Ad:
+#     ad = request.session.get(Ad, ad_id)
+#     if ad is None:
+#         raise HttpError(404, message='ad not found')
+#     return ad
+#
+# def add_ad(ad: Ad):
+#     request.session.add(ad)
+#     try:
+#         request.session.commit()
+#     except IntegrityError:
+#         raise HttpError(status_code=409, message='ad already exists with the same data')
+#
+# class AdView(MethodView):
+#     def get(self, ad_id: int):
+#         ad = get_ad(ad_id)
+#         return jsonify(ad.json)
+#
+#     def post(self):
+#         json_data = validate(CreateAd, request.json)
+#         ad = Ad(header=json_data['header'], description=json_data['password'], user_id=['user_id'])
+#         add_ad(ad)
+#         return jsonify(ad.id_json)
+#
+#     def patch(self, ad_id: int):
+#         json_data = validate(PatchAd, request.json)
+#         ad = get_ad(ad_id)
+#         if 'header' in json_data:
+#             ad.header = json_data['header']
+#         if 'description' in json_data:
+#             ad.description = json_data['description']
+#         add_ad(ad)
+#         return jsonify(ad.id_json)
+#
+#     def delete(self, ad_id: int):
+#         ad = get_ad(ad_id)
+#         request.session.delete(ad)
+#         request.session.commit()
+#         return jsonify({'status': 'advertisement delete success'})
+#
+# ad_view = AdView.as_view('ad_view')
+#
+# app.add_url_rule('/ad/<int:ad_id>',
+#                  view_func=ad_view,
+#                  methods=['GET', 'PATCH', 'DELETE']
+#                  )
+# app.add_url_rule('/ad/',
+#                  view_func=ad_view,
+#                  methods=['POST']
+#                  )
 
 
 def hello():
